1b8b805b756cb74115cd81389d30af86711cb7fd
Divyansh <divyanshbansal56@gmail.com>
2025-03-01 15:23:11 +0530

    UC2_URL

--- src/main/java/com/example/SpringIntro/HelloController.java
+++ src/main/java/com/example/SpringIntro/HelloController.java
@@ -1,12 +1,26 @@
 package com.example.SpringIntro;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
+
 public class HelloController {
-    @GetMapping("hello")
-    public String Hello(){
+
+    // UC2 - Learned to set a route and access another with it which will call the same page
+    @GetMapping(value = {"", "/", "/home"})
+    public String Hello() {
         return "Hello World!!";
     }
+
+    // UC2 - Take user input from user in url to reflet in page
+        @GetMapping("/query")
+        public String Hello(@RequestParam String name){
+            return "Hello " + name+" from Bridgelabz";
+        }
+
+
 }
